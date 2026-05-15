9cea6d47ebddf2d9cb8bc8b58c934163982b2da1
b33nj4 <bensilva2024@udec.cl>
2026-05-15 03:01:12 -0400

    primeros intentos de crear archivo o ver formas de hacerlo

--- src/main/java/informe/Informe.java
+++ src/main/java/informe/Informe.java
@@ -1,9 +1,37 @@
 package informe;
 
+import enumeraciones.*;
+import logica.*;
+
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.Date;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+
 /**
  * Clase que se encarga de generar informe sobre la reunión.
  */
 public class Informe {
+    public void crearInforme(Date fecha, Duration duracionPrevista, Instant horaPrevista,
+                             Empleado organizador, Asistencia empleadosPresentes,
+                             Duration duracionFinal, Reunion tipoReunion, Retraso retrasos, Nota notas){
 
+        try (FileWriter fw = new FileWriter("InformeReunion.txt");
+             PrintWriter escritor = new PrintWriter(fw)) {
+
+
+
+        } catch (IOException e) {
+
+            System.out.println("Ocurrió un error: " + e.getMessage());
+        }
+    }
 
 }
