ec5ad6c86453d8c5ed94d05ebf3f3da5e361f9f9
dortiz2025 <dortiz2025@inf.udec.cl>
2026-05-15 15:15:46 -0400

    Actualización Asistencia: Se agrega atributo de hora de llegada para gestionar atrasos.

--- src/main/java/logica/Asistencia.java
+++ src/main/java/logica/Asistencia.java
@@ -1,18 +1,23 @@
 package logica;
 
+import java.time.Instant;
+
 /**
  * Clase que representa la asistencia de una persona a una reunión.
  */
 public class Asistencia {
     private Persona asistente;
+    private Instant horaLlegada;
 
     /**
      * Crea una asistencia.
      *
      * @param asistente Referencia de la persona asistente.
+     * @param horaLlegada Hora en que la persona llegó a la reunión.
      */
-    public Asistencia(Persona asistente) {
+    public Asistencia(Persona asistente, Instant horaLlegada) {
         this.asistente = asistente;
+        this.horaLlegada = horaLlegada;
     }
 
     /**
@@ -31,5 +36,23 @@ public class Asistencia {
      */
     public void setAsistente(Persona asistente) {
         this.asistente = asistente;
+    }
+
+    /**
+     * Getter de horaLlegada.
+     *
+     * @return Hora de llegada a la reunión.
+     */
+    public Instant getHoraLlegada() {
+        return this.horaLlegada;
+    }
+
+    /**
+     * Setter de horaLlegada.
+     *
+     * @param horaLlegada Nueva hora de llegada a la reunión.
+     */
+    public void setHoraLlegada(Instant horaLlegada) {
+        this.horaLlegada = horaLlegada;
     }
 }
