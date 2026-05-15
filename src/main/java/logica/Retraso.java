95ca3b2e61b587942e153eb94797bb5f3b4cdc61
dortiz2025 <dortiz2025@inf.udec.cl>
2026-05-15 15:18:51 -0400

    Actualización de Retraso: Se modifica constructor. Se aclara que no se añade atributo de Hora por cambio de diseño.

--- src/main/java/logica/Retraso.java
+++ src/main/java/logica/Retraso.java
@@ -1,12 +1,14 @@
 package logica;
 
+import java.time.Instant;
+
 /**
  * Clase que representa los retrasos de la asistencia a una reunión.
  * Hereda de Asistencia.
  */
 public class Retraso extends Asistencia {
 
-    public Retraso(Persona persona) {
-        super(persona);
+    public Retraso(Persona persona, Instant horaLlegada) {
+        super(persona, horaLlegada);
     }
 }
