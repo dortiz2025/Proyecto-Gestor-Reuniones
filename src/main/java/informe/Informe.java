e402b5ed8f9f08abbfdb9ba8cf06a670dc514619
b33nj4 <bensilva2024@udec.cl>
2026-05-15 03:41:48 -0400

    condiciones para ver qué se escribirá dependiendo del tipo de reunión

--- src/main/java/informe/Informe.java
+++ src/main/java/informe/Informe.java
@@ -19,12 +19,34 @@ import java.util.List;
  * Clase que se encarga de generar informe sobre la reunión.
  */
 public class Informe {
-    public void crearInforme(Reunion reunion, Empleado organizador, Asistencia empleadosPresentes, Retraso retrasos, Nota notas){
+    /**
+     * Metodo que genera un archivo con toda la información recopilada acerca de la reunión.
+     * @param reunion información sobre la reunion.
+     */
+    public void crearInforme(Reunion reunion){
 
-        try (FileWriter fw = new FileWriter("InformeReunion.txt");
-             PrintWriter escritor = new PrintWriter(fw)) {
+        try (FileWriter fw = new FileWriter("InformeReunion.txt"); PrintWriter escritor = new PrintWriter(fw)) {
 
+            escritor.println("Organizador: " + reunion.getOrganizador());
 
+            if (reunion instanceof ReunionPresencial) {
+                escritor.println("Tipo de reunión: Presencial");
+
+            } else if (reunion instanceof ReunionVirtual) {
+                escritor.println("Tipo de reunión: Virtual");
+            }
+
+            escritor.println(("Fecha: " + reunion.getFecha() + " | Hora: " + reunion.getHora();
+
+            if (reunion instanceof ReunionPresencial) {
+                escritor.println("");
+
+            } else if (reunion instanceof ReunionVirtual) {
+                escritor.println("");
+            }
+            escritor.println("");
+            escritor.println();
+            escritor.println();
 
         } catch (IOException e) {
 
