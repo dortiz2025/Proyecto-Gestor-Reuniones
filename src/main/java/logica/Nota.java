468edbca500c81aedc2680fe013b8f983668fc4b
dortiz2025 <dortiz2025@inf.udec.cl>
2026-05-15 21:37:23 -0400

    Actualización de Nota: Se añadió constructor, getter y setter.

--- src/main/java/logica/Nota.java
+++ src/main/java/logica/Nota.java
@@ -5,4 +5,29 @@ package logica;
  */
 public class Nota {
     private String contenido;
+
+    /**
+     * Crea una nueva nota acerca de la reunión.
+     * @param contenido Contenido de la nota.
+     */
+    public Nota(String contenido) {
+        this.contenido = contenido;
+    }
+
+    /**
+     * Getter de contenido.
+     * @return Contenido de la nota.
+     */
+    public String getContenido() {
+        return contenido;
+    }
+
+    /**
+     * Setter de contenido.
+     * En caso de querer modificar anotaciones.
+     * @param contenido Nuevo contenido de la nota.
+     */
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
 }
