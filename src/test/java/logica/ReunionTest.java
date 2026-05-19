12ccaa1bbcbbf4e9644b2014dcbfd68e9f3e549f
MartinBastias <mbastias2005@gmail.com>
2026-05-19 00:40:39 -0400

    setup inicial y pruebas de inicio reunion

--- src/test/java/logica/ReunionTest.java
+++ src/test/java/logica/ReunionTest.java
@@ -1,44 +1,42 @@
 package logica;
 
-import org.junit.jupiter.api.Test;
+import excepciones.*;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.Duration;
 import java.time.Instant;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
 
 public class ReunionTest {
 
-    private Departamento depto;
     private Empleado organizador;
     private Reunion reunion;
 
     @BeforeEach
-    public void setUp() {
-        depto = new Departamento("departamento1", 15);
-        organizador = new Empleado("Manuel", "Diaz", "mdiaz@mail.com", "EMP-01", depto);
-        reunion = new Reunion(new Date(), Instant.now(), Duration.ofHours(1), organizador) {};
+    public void setUp() throws Exception {
+        organizador = new Empleado("Manuel", "Diaz", "mdiaz@mail.com", "EMP-01");
+
+        reunion = new Reunion(null, LocalDate.now(), LocalTime.now(), Duration.ofHours(1), organizador) {};
     }
 
+    //tests de inicio
+
     @Test
-    public void testIniciarAsignaHoraDeInicio() {
+    public void testIniciarAsignaHoraCorrectamente() throws Exception {
+        Instant ahora = Instant.now();
+        reunion.iniciar(ahora);
 
-        reunion.iniciar();
-
-        //verificamos que la hora de inicio se haya guardado y no sea nula
-        assertNotNull(reunion.getHoraInicio(), "La hora de inicio no debe ser nula después de iniciar la reunión");
+        assertEquals(ahora, reunion.getHoraInicio(), "La hora de inicio debe guardarse exactamente igual");
     }
-
     @Test
-    public void testFinalizarAsignaHoraDeFin() {
-
-        //verificar que empiece nula
-        assertNull(reunion.getHoraFin(), "Antes de finalizar, la hora de fin debería ser nula");
-
-        reunion.finalizar();
-
-        //comprobar
-        assertNotNull(reunion.getHoraFin(), "La hora de fin no debe ser nula después de finalizar la reunión");
+    public void testIniciarDosVecesLanzaExcepcion() throws Exception {
+        reunion.iniciar(Instant.now());
+        assertThrows(ReunionIniciadaException.class, () -> {
+            reunion.iniciar(Instant.now());
+        }, "Deberia dar error al intentar iniciar una reunion ya iniciada");
     }
 }
